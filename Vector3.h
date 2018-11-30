8aa529ee70f20bd85eb71a17ee54912d3f7b7c87
Nils Steinbuegl <nils1723@gmail.com>
2018-11-30 16:54:14 -0500

    Started setup of Vector classes

--- Vector3.h
+++ Vector3.h
@@ -7,8 +7,30 @@
 
 namespace TandenEngine {
 
-    class Vector3 {
+	class Vector3 {
+	public:
+		Vector3();
+		~Vector3();
+		// iirc, i can default individual values to something if only some inputs are given, somehow
+		Vector3(float setX, float setY, float setZ);
 
+		// Get Members
+		float GetX() { return x; };
+		float GetY() { return y; };
+		float GetZ() { return z; };
+		
+		// Set Members
+		void SetX(float newVal) { x = newVal; };
+		void SetY(float newVal) { y = newVal; };
+		void SetZ(float newVal) { z = newVal; };
+
+		// Vector Operations (dot and cross products are general, in NilsMath)
+		Vector3 operator+(const Vector3& other);
+		Vector3 operator*(const int& other);
+
+	private:
+		float x, y, z;
+	};
     };
 
 }
--- Vector4.h
+++ Vector4.h
@@ -2,36 +2,38 @@
 // Created by thomas.mcgillicuddy on 10/31/2018.
 //
 
-#ifndef HWENGINE_VECTOR2_H
-#define HWENGINE_VECTOR2_H
+#ifndef HWENGINE_VECTOR3_H
+#define HWENGINE_VECTOR3_H
 
 namespace TandenEngine {
 
-	class Vector2 {
+	class Vector4 {
 	public:
-		Vector2();
-		~Vector2();
+		Vector4();
+		~Vector4();
 		// iirc, i can default individual values to something if only some inputs are given, somehow
-		Vector2(float setX, float setY);
+		Vector4(float setX, float setY, float setZ, float setW);
 
 		// Get Members
 		float GetX() { return x; };
 		float GetY() { return y; };
-
+		float GetZ() { return z; };
+		float GetW() { return w; };
 
 		// Set Members
 		void SetX(float newVal) { x = newVal; };
 		void SetY(float newVal) { y = newVal; };
-
+		void SetZ(float newVal) { z = newVal; };
+		void SetW(float newVal) { w = newVal; };
 
 		// Vector Operations (dot and cross products are general, in NilsMath)
-		Vector2 operator+(const Vector2& other);
-		Vector2 operator*(const int& other);
+		Vector4 operator+(const Vector4& other);
+		Vector4 operator*(const int& other);
 
 	private:
-		float x, y;
+		float x, y, z, w;
 	};
 
 }
 
-#endif //HWENGINE_VECTOR2_H
+#endif //HWENGINE_VECTOR3_H
