deea84dab42bab26e762b1a021004ec822834124
Thomas McGillicuddy <thomas.mcgillicuddy@mymail.champlain.edu>
2018-12-01 14:53:42 -0500

    Adding UUID class

---
+++ UUID.cpp
@@ -0,0 +1,5 @@
+//
+// Created by thomas.mcgillicuddy on 12/1/2018.
+//
+
+#include "UUID.h"
---
+++ UUID.h
@@ -0,0 +1,14 @@
+//
+// Created by thomas.mcgillicuddy on 12/1/2018.
+//
+
+#ifndef TANDENENGINE_UUID_H
+#define TANDENENGINE_UUID_H
+
+
+class UUID {
+
+};
+
+
+#endif //TANDENENGINE_UUID_H
